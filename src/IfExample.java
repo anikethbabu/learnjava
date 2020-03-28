dd64bd518ca19d19be89a6e14bb2a0cf161a23d5
anikethbabu <anikethbabu@gmail.com>
2020-03-28 11:58:37 -0400

    Initial checkin

--- src/IfExample.java
+++ src/IfExample.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class IfExample {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Age: ");
+        int age = sc.nextInt();
+
+        if (age < 18)
+            System.out.println("Sorry, you are NOT eligible to vote");
+        else
+            System.out.println("Congrats! You are eligible to vote");
     }
 }
