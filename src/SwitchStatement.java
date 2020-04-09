c09f3912afaf16ad56e091a139127719e8a9f2dd
anikethbabu <anikethbabu@gmail.com>
2020-04-09 10:51:29 -0400

    Conditionals

---
+++ src/SwitchStatement.java
@@ -0,0 +1,17 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+    String month = "january";
+    switch(month.toLowerCase()){
+        case "january":
+            System.out.println("Jan");
+            break;
+        case "june":
+            System.out.println("June");
+            break;
+        default:
+            System.out.println("Not Sure");
+
+    }
+    }
+
+}
