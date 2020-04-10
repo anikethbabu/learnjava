ed4a9b92e9ab4e029f8a9a2d0889f1966537727c
anikethbabu <anikethbabu@gmail.com>
2020-04-10 11:10:37 -0400

    Conditionals

--- src/FlourPack.java
+++ src/FlourPack.java
@@ -1,18 +1,23 @@
 public class FlourPack {
     public static void main(String[] args) {
+        if (canPack(1, 0, 4))
+        {
+            System.out.println("Calculation is wrong");
+        }
+        if (!canPack(5, 2, 5))
+        {
+            System.out.println("Calculation is wrong");
+        }
+        }
 
-    }
+
     static boolean canPack (int bigCount, int smallCount, int goal)
     {
-        int totalCount = bigCount * 5 + smallCount;
         if (bigCount < 0 || smallCount < 0) {
             return false;
         }
-        if (totalCount >= goal)
-        {
-            return true;
-        }
-        return false;
+        int totalCount = bigCount * 5 + smallCount;
+        return (totalCount >= goal);
 
     }
 }
