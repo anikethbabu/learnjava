43d153197aa3750802dc9575b782d88aca352838
anikethbabu <anikethbabu@gmail.com>
2020-04-12 11:37:22 -0400

    Conditionals

---
+++ src/carpet/Floor.java
@@ -0,0 +1,21 @@
+package carpet;
+
+public class Floor {
+    private double length;
+    private double width;
+
+    public Floor(double length, double width) {
+        this.length = length;
+        this.width = width;
+        if (length < 0) {
+            this.length = 0;
+        }
+        if (width < 0) {
+            this.width = 0;
+        }
+    }
+    public double getArea()
+    {
+        return width * length;
+    }
+}
